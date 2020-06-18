ac264feacf614f05e2987d78c4f0c5ebd9925b7e
kiccho1101 <youodf11khp@gmail.com>
2020-06-18 09:04:45 +0900

    delete outliers

--- kaggle_m5_forecasting/eda/cv_dashboard.py
+++ kaggle_m5_forecasting/eda/cv_dashboard.py
@@ -10,7 +10,7 @@ from kaggle_m5_forecasting.data.load_data import RawData
 from kaggle_m5_forecasting.cv_dashboard import create_dashboard
 from thunderbolt import Thunderbolt
 
-TIMESTAMP = "2020-06-16_08:27:40"
+TIMESTAMP = "2020-06-17_20:57:15"
 
 cv = CVResults().from_timestamp(TIMESTAMP)
 tb = Thunderbolt("./../../resource")
@@ -18,8 +18,8 @@ raw: RawData = tb.get_data("LoadRawData")
 
 # %%
 CV_NUM = 0
-
+dir_name = f"../../output/cv/{TIMESTAMP}/{CV_NUM}"
 evaluator = cv.results[CV_NUM].get_evaluator(raw)
-create_dashboard(evaluator, raw)
-# %%
-len(evaluator.all_scores)
+create_dashboard(
+    evaluator, raw, dir_name,
+)
