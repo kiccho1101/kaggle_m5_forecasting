51e46d26ae0bd6e6fed96eb387ca6d2f74f12e75
kiccho1101 <youodf11khp@gmail.com>
2020-06-21 11:06:31 +0900

    bugfix: scaling before pca

--- kaggle_m5_forecasting/data/fe_catch22_pca.py
+++ kaggle_m5_forecasting/data/fe_catch22_pca.py
@@ -3,6 +3,7 @@ import pandas as pd
 from kaggle_m5_forecasting import M5
 from kaggle_m5_forecasting.data.make_data import MakeData
 from kaggle_m5_forecasting.data.fe_catch22 import FECatch22
+import sklearn.preprocessing
 
 
 class FECatch22PCA(M5):
@@ -15,6 +16,9 @@ class FECatch22PCA(M5):
         catch22_df: pd.DataFrame = self.load("catch22_df")
 
         catch22_df = catch22_df.drop(["percentile75", "percentile100", "std"], axis=1)
+        catch22_scaled = sklearn.preprocessing.MinMaxScaler().fit_transform(
+            catch22_df.values
+        )
 
         n_components = 3
         catch22_df = pd.concat(
@@ -22,7 +26,7 @@ class FECatch22PCA(M5):
                 catch22_df.reset_index(),
                 pd.DataFrame(
                     sklearn.decomposition.PCA(n_components=n_components).fit_transform(
-                        catch22_df
+                        catch22_scaled
                     ),
                     columns=[f"fe_catch22_pca_{i}" for i in range(n_components)],
                 ),
